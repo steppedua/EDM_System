a693ee9e2991066ba161c0d41c2587a2d9033ebb
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-12-03 16:37:28 +0300

    Project initialization

---
+++ src/main/java/greenatom/EDMSystemApplication.java
@@ -0,0 +1,11 @@
+package greenatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDMSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EDMSystemApplication.class, args);
+    }
+}
